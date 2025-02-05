5178e466d23891a34b6bc4de4c01f5f0b001f80d
Tee Kai Yang <tkaiyang2005@gmail.com>
2025-02-05 15:41:51 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Yang Do List",
+        short_name: "Yang Do List",
+        start_url: "/",
+        display: "standalone",
+        background_color: "blue",
+        theme_color: "blue",
+        icons: [
+          {
+            src: "public/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "public/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
